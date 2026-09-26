48d29190645805a9ce84617b6224f27fc1b29336
Emmanuel price <288163619+helloworld1-star@users.noreply.github.com>
2026-09-26 03:48:47 +0100

    Fix PR #212: update internal/xdrcopy/copy_test.go

--- internal/xdrcopy/copy_test.go
+++ internal/xdrcopy/copy_test.go
@@ -1,11 +1,10 @@
 package xdrcopy
 
 import (
-	"testing"
-
 	"github.com/stellar/go-stellar-sdk/xdr"
 	xdrcodec "github.com/stellar/go-xdr/xdr3"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func (u underConsume) EncodeTo(e *xdrcodec.Encoder) error {
@@ -20,26 +19,23 @@ func (u *underConsume) DecodeFrom(_ *xdrcodec.Decoder, _ uint) (int, error) {
 type underConsume struct{ V uint32 }
 
 var _ xdr.EncoderTo = (*underConsume)(nil)
+
 var _ xdr.DecoderFrom = (*underConsume)(nil)
 
 func FuzzCopy(f *testing.F) {
-	f.Add([]byte{0, 0, 0, 0})
+	// Use a simple XDR type for fuzzing.
+	val := xdr.ScVal{Type: xdr.ScValTypeScvI64, I64: new(xdr.Int64)}
+	bytes, _ := val.MarshalBinary()
+	f.Add(bytes)
+
 	f.Fuzz(func(t *testing.T, data []byte) {
-		var val xdr.ScAddress
-		err := val.UnmarshalBinary(data)
-		if err != nil {
+		var entry xdr.ScVal
+		if err := entry.UnmarshalBinary(data); err != nil {
 			return
 		}
 
-		copied, err := Copy(val)
-		if err != nil {
-			t.Errorf("copy failed: %v", err)
-			return
-		}
-
-		// Byte-identical check
-		origBytes, _ := val.MarshalBinary()
-		copyBytes, _ := copied.MarshalBinary()
-		require.Equal(t, origBytes, copyBytes)
+		copied, err := Copy(entry)
+		require.NoError(t, err)
+		require.Equal(t, entry, copied)
 	})
 }
